b962a649b77d62e334aaecc13dc21d6bf6e4cd20
FernandaFT <nanda199784@gmail.com>
2025-05-29 08:42:47 -0600

    tarea 02 avance 1

---
+++ Rincon_muebleria/src/main/java/com/rinconmuebleria/RinconmuebleriaApplication.java
@@ -0,0 +1,13 @@
+package com.rinconmuebleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinconmuebleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinconmuebleriaApplication.class, args);
+	}
+
+}
